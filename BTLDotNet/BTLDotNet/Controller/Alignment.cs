0985812f735708087ed3e5d63afbfacf9663cb96
Phạm Duy Anh <37221121+anhpd1004@users.noreply.github.com>
2018-06-01 11:55:55 +0700

    update RichTextBox

--- BTLDotNet/BTLDotNet/Controller/Alignment.cs
+++ BTLDotNet/BTLDotNet/Controller/Alignment.cs
@@ -10,6 +10,22 @@ namespace BTLDotNet.Controller
 {
     class Alignment
     {
+        public string JustifyLines(string _text, Font _font, int _ctl_width)
+        {
+            string _result = "";
+
+            // Split text in paragraphs
+            List<string> _paragraphs = new List<string>();
+            _paragraphs.AddRange(_text.Split(new[] { "\r\n" }, StringSplitOptions.None).ToList());
+
+            // Justify each paragraph and re-insert a linefeed
+            foreach (string _paragraph in _paragraphs)
+            {
+                _result += Justify(_paragraph, _font, _ctl_width) + "\r\n";
+            }
+
+            return _result.Substring(0, _result.Length - 2); // Cuts the last linefeed
+        }
         public static string JustifyParagraph(string _text, Font _font, int _ctl_width)
         {
             string _result = String.Empty;
---
+++ BTLDotNet/BTLDotNet/Controller/JustifiedRichTextBox.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTLDotNet.Controller
+{
+    public class JustifiedRichTextBox : RichTextBox
+    {
+
+        [DllImport("user32", CharSet = CharSet.Auto)]
+        private static extern int SendMessage(HandleRef hWnd, int _msg, int wParam, ref PARAFORMAT2 _pf);
+
+        [DllImport("user32", CharSet = CharSet.Auto)]
+        private static extern int SendMessage(HandleRef hWnd, int _msg, int wParam, int lParam);
+
+        public enum TextAlignment
+        {
+            Left = 1,
+            Right,
+            Center,
+            Justify
+        }
+
+        private const int EM_SETEVENTMASK = 1073;
+        private const int EM_GETPARAFORMAT = 1085;
+        private const int EM_SETPARAFORMAT = 1095;
+        private const int EM_SETTYPOGRAPHYOPTIONS = 1226;
+        private const int WM_SETREDRAW = 11;
+        private const int TO_ADVANCEDTYPOGRAPHY = 0x1;
+        private const int PFM_ALIGNMENT = 8;
+        private const int SCF_SELECTION = 1;
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct PARAFORMAT2
+        {
+            //----------------------------------------
+            public int cbSize;           // PARAFORMAT
+            public uint dwMask;
+            public short wNumbering;
+            public short wReserved;
+            public int dxStartIndent;
+            public int dxRightIndent;
+            public int dxOffset;
+            public short wAlignment;
+            public short cTabCount;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 32)]
+            public int[] rgxTabs;
+            //----------------------------------------
+            public int dySpaceBefore;   // PARAFORMAT2
+            public int dySpaceAfter;
+            public int dyLineSpacing;
+            public short sStyle;
+            public byte bLineSpacingRule;
+            public byte bOutlineLevel;
+            public short wShadingWeight;
+            public short wShadingStyle;
+            public short wNumberingStart;
+            public short wNumberingStyle;
+            public short wNumberingTab;
+            public short wBorderSpace;
+            public short wBorderWidth;
+            public short wBorders;
+        }
+
+        // SelectionAlignment can't be overriden, so step over it
+        public new TextAlignment SelectionAlignment
+        {
+            get
+            {
+                PARAFORMAT2 _pf = new PARAFORMAT2();
+                _pf.cbSize = Marshal.SizeOf(_pf);
+
+                SendMessage(new HandleRef(this, Handle), EM_GETPARAFORMAT, SCF_SELECTION, ref _pf);
+
+                // If 0, defaults to TextAlignment.Left
+                if ((_pf.dwMask & PFM_ALIGNMENT) == 0) return TextAlignment.Left;
+                return (TextAlignment)_pf.wAlignment;
+            }
+
+            set
+            {
+                PARAFORMAT2 _pf = new PARAFORMAT2();
+                _pf.cbSize = Marshal.SizeOf(_pf);
+                _pf.dwMask = PFM_ALIGNMENT;
+                _pf.wAlignment = (short)value;
+
+                SendMessage(new HandleRef(this, Handle), EM_SETPARAFORMAT, SCF_SELECTION, ref _pf);
+            }
+        }
+
+        /// Overrides OnHandleCreated to enable RTB advances options
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+
+            // EM_SETTYPOGRAPHYOPTIONS allows to enable RTB (RichEdit) Advanced Typography
+            SendMessage(new HandleRef(this, Handle), EM_SETTYPOGRAPHYOPTIONS,
+                                                     TO_ADVANCEDTYPOGRAPHY,
+                                                     TO_ADVANCEDTYPOGRAPHY);
+        }
+
+    }
+}
